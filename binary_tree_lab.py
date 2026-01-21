c5db967e6d9bcb1c0f6b6fe988849636d164c53e
Blake Godsey <somethingclever182@gmail.com>
2026-01-20 19:22:07 -0600

    initial commit

--- binary_tree_lab.py
+++ binary_tree_lab.py
@@ -8,8 +8,28 @@ class TreeNode:
 
 # TODO: Implement the max_depth function
 def max_depth(root: Optional[TreeNode]) -> int:
-    pass
+    if root is None:
+        return 0
+    
+    left_depth = max_depth(root.left)
+    right_depth = max_depth(root.right)
+
+    return 1 + max(left_depth, right_depth)
+
+
+
+
 
 # TODO: Implement the lowest_common_ancestor function
 def lowest_common_ancestor(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-    pass
+    current = root
+
+    while current is not None:
+        if p.val < current.val and q.val < current.val:
+            current = current.left
+        elif p.val > current.val and q.val > current.val:
+            current = current.right
+        else:
+            return current
+    
+    raise ValueError("Lowest common ancestor not found in the tree")
